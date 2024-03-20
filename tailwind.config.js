eac24c22876a2a95942aaadbc5c3bf67a6749887
Mehdi <mehdiek03@gmail.com>
2024-03-20 13:33:27 +0100

    Install Tailwind and Flowbite

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable*/
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{php,html,js}"]
+           ["./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
