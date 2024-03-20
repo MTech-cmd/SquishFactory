688dd99a23629de7319a3f4772276802f3734e92
Mehdi <mehdiek03@gmail.com>
2024-03-20 15:14:18 +0100

    Navbar Added a navbar styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
-/* eslint-disable*/
+/* eslint-disable */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{php,html,js}"]
-           ["./node_modules/flowbite/**/*.js"],
+  content: [
+    "./*.{php,html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
   plugins: [
     require('flowbite/plugin')
   ],
-}
+};
