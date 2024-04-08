bcd895daab938ee9df95f34f48f90209ac9dd406
Mehdi <mehdiek03@gmail.com>
2024-04-08 19:47:24 +0200

    Added basic CI/CD for server

---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+import * as bootstrap from './bootstrap.bundle.min.js';
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new Popover(popoverTriggerEl));
