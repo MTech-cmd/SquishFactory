09e83ca6cc948903e72cd2883a552fce3c5e11e9
Mehdi <mehdiek03@gmail.com>
2024-04-24 15:09:46 +0200

    No message provided

--- src/footer.php
+++ src/footer.php
@@ -1,4 +1,4 @@
 <footer class="bg-nav text-center p-1 mt-auto">
-        <p>&copy; 2021 My Mellow</p>
-        <a href="admin.php">Admin</a>
+        <p>&copy; 2024 My Mellow</p>
+        <a href="admin/login.php">Admin</a>
 </footer>
