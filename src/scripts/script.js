052b199a67c4a224eceb37a1683564c5fb588946
Mehdi <mehdiek03@gmail.com>
2024-04-26 14:26:04 +0200

    blast

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,2 @@
-import * as bootstrap from './bootstrap.bundle.js';
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new Popover(popoverTriggerEl));
